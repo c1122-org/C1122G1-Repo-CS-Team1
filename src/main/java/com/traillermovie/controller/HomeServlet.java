30b2202300c0b87474f5dd63d46c9a2e93205be4
Pope24 <110238804+Pope24@users.noreply.github.com>
2023-03-13 23:09:17 +0700

    Create detail.jsp for movie

--- src/main/java/com/traillermovie/controller/HomeServlet.java
+++ src/main/java/com/traillermovie/controller/HomeServlet.java
@@ -1,5 +1,6 @@
 package com.traillermovie.controller;
 
+import com.traillermovie.model.Director;
 import com.traillermovie.model.Movie;
 import com.traillermovie.service.homeService.HomeServiceImpl;
 
@@ -7,6 +8,7 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.sql.SQLException;
 import java.util.List;
 
 @WebServlet(name = "HomeServlet", value = "/home")
@@ -14,6 +16,29 @@ public class HomeServlet extends HttpServlet {
     private HomeServiceImpl homeService = new HomeServiceImpl();
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String action = request.getParameter("action");
+        if (action == null) {
+            action = "";
+        }
+        switch (action) {
+            case "detail":
+                try {
+                    showDetailMovie(request, response);
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+                break;
+            default:
+                showListMovie(request, response);
+                break;
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+    private void showListMovie(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         List<Movie> newMovieList = homeService.getListNewMovies();
         List<Movie> actionMovieList = homeService.getListActionMovies();
         List<Movie> popularMovieList = homeService.getListPopularMovies();
@@ -26,9 +51,13 @@ public class HomeServlet extends HttpServlet {
         RequestDispatcher dispatcher = request.getRequestDispatcher("home/home.jsp");
         dispatcher.forward(request, response);
     }
-
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+    private void showDetailMovie(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, SQLException {
+        int id = Integer.parseInt(request.getParameter("id"));
+        Movie movie = homeService.getMovieById(id);
+        List<Director> directorList = homeService.getDirectorListByIdMovie(id);
+        request.setAttribute("movie", movie);
+        request.setAttribute("directorList", directorList);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("movies/detail.jsp");
+        dispatcher.forward(request, response);
     }
 }
--- src/main/java/com/traillermovie/service/homeService/HomeServiceImpl.java
+++ src/main/java/com/traillermovie/service/homeService/HomeServiceImpl.java
@@ -1,12 +1,13 @@
 package com.traillermovie.service.homeService;
 
+import com.traillermovie.model.Director;
 import com.traillermovie.model.Movie;
 
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
-public class HomeServiceImpl implements IHomeService{
+public class HomeServiceImpl implements IHomeService {
     private String jdbcUrl = "jdbc:mysql://localhost:3306/trailler_movie";
     private String jdbcUserName = "root";
     private String jdbcPassword = "chinh@240203";
@@ -15,6 +16,11 @@ public class HomeServiceImpl implements IHomeService{
     private static final String SELECT_ALL_POPULAR_MOVIES = "select * from movies where rating_movie between 8.8 and 10;";
     private static final String SELECT_ALL_ANIMATION_MOVIES = "select * from movies where id_genre = 4;";
     private static final String SELECT_ALL_NEW_MOVIES = "select * from movies where year_movie > 2004 order by year_movie desc;";
+    private static final String SELECT_MOVIE_BY_ID = "select * from movies where id_movie = ?;";
+    private static final String SELECT_DIRECTOR_BY_ID_MOVIE = "select dr.id_director, dr.name_director, image_director from directors as dr\n" +
+            "join movie_director as md on dr.id_director = md.id_director\n" +
+            "where md.id_movie = ?;";
+
     protected Connection getConnection() {
         Connection connection = null;
         try {
@@ -27,9 +33,10 @@ public class HomeServiceImpl implements IHomeService{
         }
         return connection;
     }
+
     public List<Movie> getListActionMovies() {
         List<Movie> actionMovieList = new ArrayList<>();
-        try(Connection connection = getConnection(); PreparedStatement preparedStatement = connection.prepareStatement(SELECT_ALL_ACTION_MOVIES)) {
+        try (Connection connection = getConnection(); PreparedStatement preparedStatement = connection.prepareStatement(SELECT_ALL_ACTION_MOVIES)) {
             ResultSet resultSet = preparedStatement.executeQuery();
             while (resultSet.next()) {
                 int id = resultSet.getInt("id_movie");
@@ -47,9 +54,10 @@ public class HomeServiceImpl implements IHomeService{
         }
         return actionMovieList;
     }
+
     public List<Movie> getListPopularMovies() {
         List<Movie> popularMovieList = new ArrayList<>();
-        try(Connection connection = getConnection(); PreparedStatement preparedStatement = connection.prepareStatement(SELECT_ALL_POPULAR_MOVIES)) {
+        try (Connection connection = getConnection(); PreparedStatement preparedStatement = connection.prepareStatement(SELECT_ALL_POPULAR_MOVIES)) {
             ResultSet resultSet = preparedStatement.executeQuery();
             while (resultSet.next()) {
                 int id = resultSet.getInt("id_movie");
@@ -67,9 +75,10 @@ public class HomeServiceImpl implements IHomeService{
         }
         return popularMovieList;
     }
+
     public List<Movie> getListAnimationMovies() {
         List<Movie> animationMovieList = new ArrayList<>();
-        try(Connection connection = getConnection(); PreparedStatement preparedStatement = connection.prepareStatement(SELECT_ALL_ANIMATION_MOVIES)) {
+        try (Connection connection = getConnection(); PreparedStatement preparedStatement = connection.prepareStatement(SELECT_ALL_ANIMATION_MOVIES)) {
             ResultSet resultSet = preparedStatement.executeQuery();
             while (resultSet.next()) {
                 int id = resultSet.getInt("id_movie");
@@ -91,7 +100,7 @@ public class HomeServiceImpl implements IHomeService{
     @Override
     public List<Movie> getListNewMovies() {
         List<Movie> newMovieList = new ArrayList<>();
-        try(Connection connection = getConnection(); PreparedStatement preparedStatement = connection.prepareStatement(SELECT_ALL_NEW_MOVIES)) {
+        try (Connection connection = getConnection(); PreparedStatement preparedStatement = connection.prepareStatement(SELECT_ALL_NEW_MOVIES)) {
             ResultSet resultSet = preparedStatement.executeQuery();
             while (resultSet.next()) {
                 int id = resultSet.getInt("id_movie");
@@ -108,5 +117,42 @@ public class HomeServiceImpl implements IHomeService{
             throw new RuntimeException(e);
         }
         return newMovieList;
+    }
+
+    @Override
+    public Movie getMovieById(int id) throws SQLException {
+        Movie movie = null;
+        try (Connection connection = getConnection(); PreparedStatement preparedStatement = connection.prepareStatement(SELECT_MOVIE_BY_ID)) {
+            preparedStatement.setInt(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                String title = resultSet.getString("title_movie");
+                double rating = resultSet.getDouble("rating_movie");
+                int rank = resultSet.getInt("rank_movie");
+                int yearPublic = resultSet.getInt("year_movie");
+                String image = resultSet.getString("image_movie");
+                String description = resultSet.getString("description_movie");
+                String trailer = resultSet.getString("traller_movie");
+                movie = new Movie(id, title, rating, rank, yearPublic, image, description, trailer);
+            }
+            return movie;
+        }
+    }
+
+    public List<Director> getDirectorListByIdMovie(int id) throws SQLException {
+        List<Director> directorList = new ArrayList<>();
+        try (Connection connection = getConnection(); PreparedStatement preparedStatement = connection.prepareStatement(SELECT_DIRECTOR_BY_ID_MOVIE)) {
+            preparedStatement.setInt(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                int id_director = resultSet.getInt("dr.id_director");
+                String name = resultSet.getString("dr.name_director");
+                String image = resultSet.getString("dr.image_director");
+                directorList.add(new Director(id_director, name, image));
+            }
+            return directorList;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
